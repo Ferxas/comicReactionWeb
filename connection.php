7936c7a80f5884925bef676613a132c64a1fbe99
Ferxas <ferxas321@hotmail.es>
2023-04-18 01:28:30 +0000

    new comment

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'dbname';
+$dbname = 'dbname'; // settings
 
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
